e292d330af4cbcfe9e0364678dd03858343c2313
Aakash Dhar <aakash.dhar@simteq.com>
2018-04-07 10:17:00 +0530

    Added Dom PDF
    - Added DOM pdf to composer file
    - Added DOM pdf to config\app  file

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Collective\Html\HtmlServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
     ],
 
@@ -212,6 +213,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
